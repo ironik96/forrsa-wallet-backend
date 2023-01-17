c5fd2a6e0762dde239116e94fd9fb94531dfbd97
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2023-01-17 22:20:42 +0300

    Add omar cors file but still not working

---
+++ src/main/java/com/kfh/forrsawalletbackend/RestConfig.java
@@ -0,0 +1,14 @@
+package com.kfh.forrsawalletbackend;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class RestConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200");
+    }
+}
